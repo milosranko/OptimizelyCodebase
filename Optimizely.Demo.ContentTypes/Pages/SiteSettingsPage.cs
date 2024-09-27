63125b2d369cdfc70432b701a706b647cd3ff3d4
Miloš Ranković <milos.ranko@live.com>
2024-09-27 21:38:16 +0200

    Adjusted page type availability

--- Optimizely.Demo.ContentTypes/Pages/SiteSettingsPage.cs
+++ Optimizely.Demo.ContentTypes/Pages/SiteSettingsPage.cs
@@ -8,7 +8,12 @@ namespace Optimizely.Demo.ContentTypes.Pages;
 [ContentType(
     GUID = "{7A553D60-F2DF-4094-A650-8812F755EF6E}",
     GroupName = Globals.TabNames.Specialized)]
-[AvailableContentTypes(Availability.None)]
+[AvailableContentTypes(
+    Availability.None,
+    IncludeOn =
+    [
+        typeof(StartPage)
+    ])]
 public class SiteSettingsPage : SiteSettingsPageBase
 {
     #region Content tab
